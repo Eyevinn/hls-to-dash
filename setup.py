b14321196210ad94fa68d4ec8193a0321f1eab86
birme <jonas.birme@eyevinn.se>
2016-08-11 16:58:55 +0200

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+from os.path import dirname, abspath, join, exists
 from setuptools import setup
+
+install_reqs = [reg for req in open(abspath(join(dirname(__file__), 'requirements.txt')))]
 
 setup(
     name = "hls2dash",
@@ -7,6 +10,7 @@ setup(
     author_email = "jonas.birme@eyevinn.se",
     description = ("Command line tools for HLS to MPEG DASH repackaging"),
     license = "MIT",
+    install_requires=install_reqs,
     url = "https://github.com/Eyevinn/hls-to-dash",
     packages = ['python', 'python/lib'],
     scripts = ['bin/hls-to-dash']
