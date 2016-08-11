572170b9346ea181ebc7f007f0c5016f85f70ad1
birme <jonas.birme@eyevinn.se>
2016-08-11 16:52:52 +0200

    Added LICENSE info

--- python/hls-to-dash.py
+++ python/hls-to-dash.py
@@ -1,4 +1,6 @@
-# Depends on patched mp4packager (Bento4)
+# Copyright 2016 Eyevinn Technology. All rights reserved
+# Use of this source code is governed by a MIT License
+# license that can be found in the LICENSE file.
 # Author: Jonas Birme (Eyevinn Technology)
 
 import argparse
--- python/lib/MPDRepresentation.py
+++ python/lib/MPDRepresentation.py
@@ -1,3 +1,9 @@
+# Copyright 2016 Eyevinn Technology. All rights reserved
+# Use of this source code is governed by a MIT License
+# license that can be found in the LICENSE file.
+# Author: Jonas Birme (Eyevinn Technology)
+
+
 class Segment:
     def __init__(self, duration, isFirst):
         self.duration = duration
