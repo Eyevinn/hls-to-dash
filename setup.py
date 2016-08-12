a234b72e8f341a25512b96cd5679b76632749a93
birme <jonas.birme@eyevinn.se>
2016-08-12 06:16:56 +0200

    Automatically generate long description from markdown

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from os.path import dirname, abspath, join, exists
 from setuptools import setup
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+long_description = None
+if exists("README.md"):
+    long_description = read_md("README.md")
+
 install_reqs = [req for req in open(abspath(join(dirname(__file__), 'requirements.txt')))]
 
 setup(
@@ -8,7 +19,8 @@ setup(
     version = "0.1.0",
     author = "Jonas Birme",
     author_email = "jonas.birme@eyevinn.se",
-    description = ("Command line tools for HLS to MPEG DASH repackaging"),
+    description = "Command line tools for HLS to MPEG DASH repackaging",
+    long_description=long_description,
     license = "MIT",
     install_requires=install_reqs,
     url = "https://github.com/Eyevinn/hls-to-dash",
